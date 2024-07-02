e6da3bc4d26864fe0e3a81897c6cf1fadf89b2e2
NeroKuroko <149225311+NeroKuroko@users.noreply.github.com>
2024-07-01 21:18:07 -0600

    Errors index.js

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const createError = require('http-errors'); // Importa el módulo createError
 
-//Conexión con la base de datos 
+// Conexión con la base de datos
 mongoose
     // Conectado de manera local
     //.connect('mongodb://127.0.0.1:27017/libros')
-
     .connect('mongodb+srv://gabrielaszti21:1234@libros.joxikaj.mongodb.net/libros?retryWrites=true&w=majority&appName=libros')
-
     .then((x) => {
-        console.log(`Conectado exitosamente a la base ${x.connections[0].name}`)
+        console.log(`Conectado exitosamente a la base ${x.connections[0].name}`);
     })
-    .catch((error) =>{
-        console.log('Error de conexión: ',error.reason)
-    })
+    .catch((error) => {
+        console.log('Error de conexión: ', error.reason);
+    });
 
+// Configuración del servidor web
+const libroRouter = require('./routes/libro.routes');
+const app = express();
 
-//Configuración del servidor web 
-const libroRouter = require('./routes/libro.routes')
-const app = express()
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
-        extended : false
+        extended: false,
     })
-)
+);
 
-app.use(cors())
+app.use(cors());
 
-app.use('/api',libroRouter)
+app.use('/api', libroRouter);
 
-//Habilirar el puerto 
-const port = process.env.PORT || 4000
+// Habilitar el puerto
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
-    console.log('Escuchando en el puerto '+port)
-})
+    console.log('Escuchando en el puerto ' + port);
+});
 
-//Manejar posibles errores 404
-app.use((req, res, next) =>{
-    next(createError(404))
-})
+// Manejar posibles errores 404
+app.use((req, res, next) => {
+    next(createError(404));
+});
 
 // Manejar errores
-app.use(function(err, req, res, next){
-    console.log(err.message)
-    if(!err.statusCode) err.statusCode = 500
-    res.statud(err.statusCode).send(err.message)
-})
+app.use(function (err, req, res, next) {
+    console.log(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message); // Corrección de la función res.statud a res.status
+});
